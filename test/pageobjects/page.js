253fa83ffb40deb6dbd9fb3ee367631ebd79764e
Eddie Saunders <saunders.eddie@outlook.com>
2021-12-09 07:31:02 -0500

    POM updates

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,6 +1,21 @@
 module.exports = class Page {
 
+    get menu() {
+        return $("//button[@id='react-burger-menu-btn']");
+    }
+
+    get menuLogout() {
+        return $("//a[@id='logout_sidebar_link']");
+    }
+
     async openURL(pageURL) {
         return await browser.url(pageURL);
+    }
+
+    async logout() {
+        await this.menu.click();
+        await this.menuLogout.waitForClickable({timeout: 3000});
+        await this.menuLogout.click();
+        return
     }
 }
--- test/specs/User-Tests/User-FailedLogin.js
+++ test/specs/User-Tests/User-FailedLogin.js
@@ -1,9 +1,25 @@
+const loginPage = require('../../pageobjects/loginPage');
 const LoginPage = require ('../../pageobjects/loginPage');
 
 describe("Testing Sub Folders in specs", async () => {
-    it ("Lets just open the browser", async () => {
+    beforeEach (() => {
         LoginPage.open()
+    })
+
+    it ("Login with good user credentials to test", async () => {
         LoginPage.userLogin('standard_user', 'secret_sauce');
+        await LoginPage.menu.click();
+        await LoginPage.menuLogout.waitForClickable({timeout: 3000});
+        await LoginPage.menuLogout.click();
+        // This POM no longer works, but works outside of the class
+        // Needs further evaluation and testing on other browers
+        // LoginPage.logout();
         await browser.pause(5000);
     })
+    
+    xit ("Loop though users and attemp login", async () => {
+        console.log ("Opening Loop test")
+        await browser.pause(5000);
+    })
+
 })
